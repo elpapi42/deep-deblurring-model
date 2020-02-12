79edf5b578b732d71ccc71daabd3f7e81eb63c07
ElPapi42 <whitman-2@hotmail.com>
2020-02-12 13:43:01 -0400

    Add generate_csv script

---
+++ deblurrer/scripts/datasets/generate_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding=utf-8
+
+"""
+Use all the available data for generate a .csv with blur/sharp images paths.
+
+Every folder must contain two subfolder with the respective shar/blur images:
+
+- <dataset_name>
+-- sharp
+--- img01.jpg
+--- img02.jpg
+-- blur
+--- img01.jpg
+--- img02.jpg
+
+The image pairs must have the same name inside different folders
+img01.jpg is present in blur and sharp folders
+
+"""
+
+
+if (__name__ == '__main__'):
+    pass
