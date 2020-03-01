09a560271e55d289363d2c058fba39e77e32df12
ElPapi42 <whitman-2@hotmail.com>
2020-03-01 17:21:20 -0400

    Added mock losses tests

--- tests/model/losses_test.py
+++ tests/model/losses_test.py
@@ -2,3 +2,15 @@
 # coding=utf-8
 
 """Test suit for custom losses module."""
+
+import pytest
+
+from deblurrer.model.losses import ragan_ls_loss, generator_loss
+
+
+def test_ragan_ls_loss():
+    assert ragan_ls_loss is not None
+
+
+def test_generator_loss():
+    assert generator_loss is not None
