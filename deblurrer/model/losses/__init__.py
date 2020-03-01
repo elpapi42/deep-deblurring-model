07e0a7e74b9299b9f229dadd63013c8b74ff9a88
ElPapi42 <whitman-2@hotmail.com>
2020-03-01 15:42:55 -0400

    Added losses Subpackage and basic files

---
+++ deblurrer/model/losses/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# coding=utf-8
+
+"""Defines the custom loss functions used at train time."""
---
+++ deblurrer/model/losses/discriminator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding=utf-8
+
+"""
+Defines the cutom loss function for discriminator.
+
+It is a two-term binary crossextrpy loss
+each one associated with one of the DoubleScaleDiscriminator heads
+Author call it RaGAN-LS loss
+"""
