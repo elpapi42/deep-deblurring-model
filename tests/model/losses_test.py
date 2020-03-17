660ad034302b60784da0abe872a448813134f426
ElPapi42 <whitman-2@hotmail.com>
2020-03-17 14:29:17 -0400

    Fixed comparison, now it compares two tensors instead of relying on eager execution

--- tests/model/losses_test.py
+++ tests/model/losses_test.py
@@ -4,6 +4,7 @@
 """Test suit for custom losses module."""
 
 import tensorflow as tf
+import numpy as np
 
 from deblurrer.model.losses import ragan_ls_loss
 from deblurrer.model.losses import discriminator_loss
@@ -15,7 +16,7 @@ def test_ragan_ls_loss():
 
     loss = ragan_ls_loss(pred, real_preds=True)
 
-    assert loss == 0.22625
+    assert loss == tf.constant(0.22625)
     assert loss.shape == []
 
 
@@ -26,7 +27,7 @@ def test_discriminator_loss():
     loss = discriminator_loss(real_pred, fake_pred)
 
     assert loss.shape == []
-    assert loss == 1.7675
+    assert loss.numpy() == tf.constant(1.7675)
 
 
 def test_generator_loss():
@@ -35,4 +36,4 @@ def test_generator_loss():
     loss = generator_loss(fake_pred)
 
     assert loss.shape == []
-    assert loss == 0.2832668
+    assert loss.numpy() == tf.constant(0.2832668)
