33dddf5a4f82d1ccbf2e64dc1536e3b31ecc92d1
ElPapi42 <whitman-2@hotmail.com>
2020-03-24 13:10:41 -0400

    add tests for Tester methods

--- tests/scripts/training/tester_test.py
+++ tests/scripts/training/tester_test.py
@@ -45,7 +45,8 @@ def test_step_fn(dataset):
         for batch in dataset.take(1):
             gen_loss, disc_loss = tester.step_fn(batch)
 
-    assert gen_loss == 0.45626307
+    assert tf.cast(gen_loss, dtype=tf.float16) == 0.4562630
+    assert disc_loss == 4.992206
 
 
 def test_distributed_step(dataset):
@@ -62,3 +63,40 @@ def test_distributed_step(dataset):
             gen_loss, disc_loss = tester.distributed_step(batch)
 
     assert gen_loss == 0.405701
+    assert disc_loss == 5.000844
+
+
+def test_gan_forward_pass(dataset):
+    strategy = tf.distribute.OneDeviceStrategy(device='/cpu:0')
+
+    with strategy.scope():
+        tester = Tester(
+            FPNGenerator(16),
+            DoubleScaleDiscriminator(),
+            strategy,
+        )
+
+        for batch in dataset.take(1):
+            input_images = batch.get('sharp')
+            real_out, fake_out, gen_images = tester.gan_forward_pass(batch)
+
+    assert gen_images.shape == input_images.shape
+    assert real_out['local'].shape == [2, 1]
+    assert fake_out['global'].shape == [2, 1]
+    assert fake_out['global'][0][0] == 0.48832405
+
+
+def test_get_loss_network(dataset):
+    strategy = tf.distribute.OneDeviceStrategy(device='/cpu:0')
+
+    with strategy.scope():
+        tester = Tester(
+            FPNGenerator(16),
+            DoubleScaleDiscriminator(),
+            strategy,
+        )
+
+        loss_network = tester.get_loss_network()
+
+    assert len(loss_network.layers) == 10
+    assert loss_network.get_layer(index=-1).name == 'block3_conv3'
