a2dc9f927d455e708c24e66635fc361e92f380c9
ElPapi42 <whitman-2@hotmail.com>
2020-04-08 09:56:03 -0400

    Added variable conv block count for global discriminator
    This will allow for more flexibility trough hyperprams

--- deblurrer/model/discriminator/dglobal.py
+++ deblurrer/model/discriminator/dglobal.py
@@ -7,10 +7,12 @@ Global Discriminator side of the Discriminator.
 Analize the full image for prdict if its generated or not
 """
 
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, Model
 
 from deblurrer.model.discriminator import LeakyConvBlock
+from deblurrer.model.generator import ConvBlock
 
 
 class GlobalDiscriminator(Model):
@@ -20,21 +22,40 @@ class GlobalDiscriminator(Model):
     Analize the full image for prdict if its generated or not
     """
 
-    def __init__(self, alpha=0.2):
+    def __init__(self, filters=12, conv_count=4, alpha=0.2):
         """
         Init the layers of the model.
 
         Args:
+            filters (int): Base number of filters, scaled by conv_count
+            conv_count (int): Number of leaky conv layers to use
             alpha (float): LeakyReLU alpha param
         """
         super().__init__()
 
-        self.conv_a = LeakyConvBlock(32, 5, 2, alpha, True)
-        self.conv_b = LeakyConvBlock(64, 5, 4, alpha)
-        self.conv_c = LeakyConvBlock(128, 3, 2, alpha)
-        self.conv_d = LeakyConvBlock(256, 3, 2, alpha)
-        self.conv_e = LeakyConvBlock(512, 2, 2, alpha)
-        self.conv_f = layers.Conv2D(64, 2, 2, padding='same')
+        self.conv_in = ConvBlock(filters, 5, 2, override_activation=True)
+
+        # Interpolate kernel size from 5 to 2 over all the layers
+        kernels = np.interp(
+            x=[index / (conv_count - 1) for index in range(conv_count)],
+            xp=[0.0, 1.0],
+            fp=[5.0, 2.0],
+        )
+        kernels = np.round(kernels).astype(int)
+
+        # Stack the con layers into conv_layers
+        self.conv_layers = []
+        for index, kernel in zip(range(2, conv_count + 2), kernels):
+            self.conv_layers.append(
+                LeakyConvBlock(
+                    filters=index * filters,
+                    kernel_size=(kernel, kernel),
+                    strides=2,
+                    alpha=alpha,
+                ),
+            )
+
+        self.conv_out = layers.Conv2D(64, 2, 2, padding='same')
 
         self.global_pool = layers.GlobalMaxPool2D()
         self.dense = layers.Dense(1, activation='sigmoid')
@@ -50,12 +71,12 @@ class GlobalDiscriminator(Model):
             Probabilities Tensor of shape [batch, 1]
         """
         # Conv feedforward
-        outputs = self.conv_a(inputs)
-        outputs = self.conv_b(outputs)
-        outputs = self.conv_c(outputs)
-        outputs = self.conv_d(outputs)
-        outputs = self.conv_e(outputs)
-        outputs = self.conv_f(outputs)
+        outputs = self.conv_in(inputs)
+
+        for conv in self.conv_layers:
+            outputs = conv(outputs)
+
+        outputs = self.conv_out(outputs)
 
         # Sigmoid activation function
         outputs = self.global_pool(outputs)
