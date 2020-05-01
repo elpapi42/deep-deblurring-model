107a508a4595e177ba2f08c0ba477194d2f8cba1
ElPapi42 <whitman-2@hotmail.com>
2020-04-30 22:04:20 -0400

    Disable losses concrete values testing

--- tests/model/losses_test.py
+++ tests/model/losses_test.py
@@ -12,30 +12,26 @@ from deblurrer.model.losses import generator_loss
 from deblurrer.model.losses import feature_reconstruction_loss
 from tests.fixtures import loss_network
 
-"""
+
 def test_ragan_ls_loss():
     pred = tf.constant([[0.5], [0.55]])
 
     loss = ragan_ls_loss(pred, real_preds=True)
 
-    assert loss == 0.22625
+    #assert loss == 0.22625
     assert loss.shape == []
 
 
 def test_discriminator_loss():
-    real_pred = {
+    pred = {
         'local': tf.constant([[0.75, 0.5, 0.95]]),
         'global': tf.constant([[0.75, 0.5, 0.95]]),
     }
-    fake_pred = {
-        'local': tf.constant([[0.15, 0.45, 0.25]]),
-        'global': tf.constant([[0.75, 0.5, 0.95]]),
-    }
 
-    loss = discriminator_loss(real_pred, fake_pred)
+    loss = discriminator_loss(pred, True)
 
     assert loss.shape == []
-    assert loss == 4.9108334
+    #assert loss == 4.9108334
 
 
 def test_generator_loss(loss_network):
@@ -51,8 +47,8 @@ def test_generator_loss(loss_network):
     loss = generator_loss(gen_input, sharp_input, fake_pred, loss_network)
 
     assert loss.shape == []
-    assert tf.cast(loss, dtype=tf.float16) == 0.08987017
-"""
+    #assert tf.cast(loss, dtype=tf.float16) == 0.08987017
+
 
 def test_feature_reconstruction_loss(loss_network):
     # Fake image, will be generated and sharp image
