affee7a0e774fff5001773c325da18e1ddfa05c3
ElPapi42 <whitman-2@hotmail.com>
2020-06-04 12:16:36 -0400

    Add convert function

--- deblurrer/scripts/deploy.py
+++ deblurrer/scripts/deploy.py
@@ -15,6 +15,20 @@ from dotenv import load_dotenv
 from deblurrer.model.wrapper import ImageByteWrapper
 from deblurrer.scripts.train import run as train
 
+import logging
+logging.getLogger("tensorflow").setLevel(logging.DEBUG)
+
+try:
+  # %tensorflow_version only exists in Colab.
+  import tensorflow.compat.v2 as tf
+except Exception:
+  pass
+tf.enable_v2_behavior()
+
+from tensorflow import keras
+import numpy as np
+import pathlib
+
 
 def wrap(gan):
     """
@@ -40,12 +54,34 @@ def wrap(gan):
     
     return wrapped_generator
 
+def convert(model):
+    """
+    Converts the supplied model into tflite format.
+
+    Applies quantization.
+
+    Args:
+        model (tf.keras.Model): Model to be converted yo tflite model
+
+    Returns:
+        quantized tflite model
+    """
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+    tflite_model = converter.convert()
+
+    return tflite_model
 
 
 def run(gan):
     generator = wrap(gan)
 
+    tflite_gen = convert(generator)
+
+    return tflite_gen
+
 if (__name__ == '__main__'):
+    """
     path = os.path.dirname(
         os.path.dirname(
             os.path.dirname(
@@ -72,7 +108,35 @@ if (__name__ == '__main__'):
         output_folder=output_path,
         logs_folder=logs_path,
     )
+    """
 
-    generator = wrap(model)
+    # Load MNIST dataset
+    mnist = keras.datasets.mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-    generator.summary()
+    # Normalize the input image so that each pixel value is between 0 to 1.
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    # Define the model architecture
+    model = keras.Sequential([
+        keras.layers.InputLayer(input_shape=(28, 28)),
+        keras.layers.Reshape(target_shape=(28, 28, 1)),
+        keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation=tf.nn.relu),
+        keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    # Train the digit classification model
+    model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+    model.fit(
+        train_images,
+        train_labels,
+        epochs=1,
+        validation_data=(test_images, test_labels)
+    )
+
+    generator = convert(model)
